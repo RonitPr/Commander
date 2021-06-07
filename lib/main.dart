1f31cb52266c0bbc43dad8b91532f3a2074d7421
RonitPriav <42570726+RonitPr@users.noreply.github.com>
2021-06-07 15:32:35 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Commander',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Directionality(
+        textDirection: TextDirection.rtl,
+        child: MyHomePage(title: 'קומנדר'),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        shadowColor: Colors.transparent,
+      ),
+      body: Center(
+        child: Text("גוף העמוד"),
+      ),
+    );
+  }
+}
